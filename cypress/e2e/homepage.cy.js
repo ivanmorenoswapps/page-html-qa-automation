4d4e7249e8e52f078522daaa1fff00b69939882d
Iván Moreno <cmoreno@swapps.com>
2025-07-28 17:00:44 -0500

    config cypress

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,11 +1,7 @@
 describe('Visual Home Page', () => {
   it('Debería coincidir visualmente la página de inicio', () => {
     cy.visit('https://ivanmorenoswapps.github.io/page-html-qa-automation/');
-    cy.get('header', { timeout: 10000 }).should('be.visible');
-    cy.wait(1000);
-    cy.scrollTo('bottom');
-    cy.get('footer', { timeout: 10000 }).should('be.visible');
     cy.wait(4000);
-    cy.percySnapshot('Homepage Example - con scroll');
+    cy.percySnapshot('Homepage Example');
   });
 });
